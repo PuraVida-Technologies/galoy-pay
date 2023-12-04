0ccf0847d2cd97d36b296b4a3a5bb5661779453d
Luke Chavers <luke@puravidabitcoin.io>
2023-12-03 18:17:19 -0600

    PuraVida - Updated config.ts values

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,16 @@
 export const USD_INVOICE_EXPIRE_INTERVAL = 60 * 5
 export const MAX_INPUT_VALUE_LENGTH = 14
-export const CASH_REGISTER_DESCRIPTION = "Bitcoin Beach (Blink) POS Cash Register"
-export const APP_DESCRIPTION = "Bitcoin Beach (Blink) official lightning network node"
-export const URL_HOST_DOMAIN = "blink.sv"
+
+/*
+--- PuraVida Modifications ---------------------------------------------------------------------------------------------
+
+  5. Updated config values - LC 12/3/23
+
+------------------------------------------------------------------------------------------------------------------------
+*/
+export const CASH_REGISTER_DESCRIPTION = "Pay PuraVida"
+export const APP_DESCRIPTION = "PuraVida Official Lightning Network Node"
+export const URL_HOST_DOMAIN = "pay.puravidabitcoin.io"
+/*
+--- /PuraVida Modifications --------------------------------------------------------------------------------------------
+*/
